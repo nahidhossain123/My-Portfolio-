fa19c640e76e15e23d0a0e222d1c13a15dea6ec5
Nahid Hossain <nahidhossain351@gmail.com>
2026-04-11 22:30:26 +0600

    update(Experience): update experience section design

--- src/component/Experience.tsx
+++ src/component/Experience.tsx
@@ -126,50 +126,73 @@ const Experience: React.FC = () => {
                 </h2>
 
                 {/* --- Project Grid --- */}
-                <div className="space-y-6">
-                    <div>
-                        <h3 className="text-xl font-semibold">
-                            Aspyrer.com, Canada
-                        </h3>
+                <div className="space-y-5 flex flex-col justify-center items-center">
+                    <div className='flex'>
+                        <div className='space-y-5 border border-white w-full md:max-w-[40%] p-5 rounded-xl -rotate-5 flex-start'>
+                            <div className="absolute -top-8 left-1/2 -translate-x-1/2">
+                                <div className="w-5 h-5 bg-red-500 rounded-full shadow-md relative">
+                                    <div className="absolute inset-0 bg-red-700 rounded-full scale-75"></div>
+                                </div>
+                                <div className="w-1 h-6 bg-gray-600 mx-auto"></div>
+                            </div>
+                            <h3 className="text-5xl font-semibold">
+                                Aspyrer.com, Canada
+                            </h3>
 
-                        <p className="text-sm uppercase tracking-wide text-gray-500 dark:text-gray-300">
-                            Front-End Developer (Remote) • Jan 2023 — Present
-                        </p>
+                            <p className="text-sm uppercase tracking-wide text-gray-500 dark:text-gray-300">
+                                Front-End Developer (Remote) • Jan 2023 — Present
+                            </p>
 
-                        <p className="mt-3 text-sm text-gray-600 leading-relaxed dark:text-gray-300">
-                            Worked on API integration, UI design, feature development, bug fixing, and performance optimization
-                            across both React.js web applications and React Native mobile apps.
-                        </p>
+                            <p className="mt-3 text-sm text-gray-600 leading-relaxed dark:text-gray-300">
+                                Worked on API integration, UI design, feature development, bug fixing, and performance optimization
+                                across both React.js web applications and React Native mobile apps.
+                            </p>
+                        </div>
                     </div>
-                    <div>
-                        <h3 className="text-xl font-semibold">
-                            Dynamic Software Ltd.
-                        </h3>
+                    <div className='flex justify-end '>
+                        <div className='space-y-5 border border-white w-full md:max-w-[40%] p-5 rounded-xl rotate-5'>
+                            <div className="absolute -top-8 left-1/2 -translate-x-1/2">
+                                <div className="w-5 h-5 bg-red-500 rounded-full shadow-md relative">
+                                    <div className="absolute inset-0 bg-red-700 rounded-full scale-75"></div>
+                                </div>
+                                <div className="w-1 h-6 bg-gray-600 mx-auto"></div>
+                            </div>
+                            <h3 className="text-5xl font-semibold">
+                                Dynamic Software Ltd.
+                            </h3>
 
-                        <p className="text-sm uppercase tracking-wide text-gray-500 dark:text-gray-300">
-                            Front-End Software Engineer • Apr 2022 — Jan 2023
-                        </p>
+                            <p className="text-sm uppercase tracking-wide text-gray-500 dark:text-gray-300">
+                                Front-End Software Engineer • Apr 2022 — Jan 2023
+                            </p>
 
-                        <p className="mt-3 text-sm text-gray-600 leading-relaxed dark:text-gray-300">
-                            Converted Figma designs into responsive user interfaces, developed and customized WordPress themes,
-                            and collaborated on Laravel + React-based applications to build scalable frontend features.
-                        </p>
+                            <p className="mt-3 text-sm text-gray-600 leading-relaxed dark:text-gray-300">
+                                Converted Figma designs into responsive user interfaces, developed and customized WordPress themes,
+                                and collaborated on Laravel + React-based applications to build scalable frontend features.
+                            </p>
+                        </div>
                     </div>
-                    <div>
-                        <h3 className="text-xl font-semibold">
-                            Trenza Softwares
-                        </h3>
+                    <div className='flex'>
+                        <div className='space-y-5 border border-white w-full md:max-w-[40%] p-5 rounded-xl -rotate-5'>
+                            <div className="absolute -top-8 left-1/2 -translate-x-1/2">
+                                <div className="w-5 h-5 bg-red-500 rounded-full shadow-md relative">
+                                    <div className="absolute inset-0 bg-red-700 rounded-full scale-75"></div>
+                                </div>
+                                <div className="w-1 h-6 bg-gray-600 mx-auto"></div>
+                            </div>
+                            <h3 className="text-5xl font-semibold">
+                                Trenza Softwares
+                            </h3>
 
-                        <p className="text-sm uppercase tracking-wide text-gray-500 dark:text-gray-400">
-                            Trainee Software Engineer • Nov 2021 — Apr 2022
-                        </p>
+                            <p className="text-sm uppercase tracking-wide text-gray-500 dark:text-gray-400">
+                                Trainee Software Engineer • Nov 2021 — Apr 2022
+                            </p>
 
-                        <p className="mt-3 text-sm text-gray-600 leading-relaxed dark:text-gray-400">
-                            Translated Figma designs into responsive user interfaces and assisted in developing and customizing
-                            WordPress themes, gaining hands-on experience in frontend development workflows.
-                        </p>
+                            <p className="mt-3 text-sm text-gray-600 leading-relaxed dark:text-gray-400">
+                                Translated Figma designs into responsive user interfaces and assisted in developing and customizing
+                                WordPress themes, gaining hands-on experience in frontend development workflows.
+                            </p>
+                        </div>
                     </div>
-
                 </div>
             </div>
         </section>
